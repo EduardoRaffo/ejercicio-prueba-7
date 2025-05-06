c3fff05aacab63d3b71ff10f2ca8e8e4d64b5285
EduardoRaffo <eduardoraffo26@gmail.com>
2025-05-06 23:22:30 +0200

    Update scraping_wiki.py

--- scraping_wiki.py
+++ scraping_wiki.py
@@ -3,10 +3,13 @@ import sqlite3
 import time
 from bs4 import BeautifulSoup
 import re
-#Intamos la conexión con la libreria requests, considerando que si algo falla se vea el error.
+
+# Intamos la conexión con la libreria requests, considerando que si algo falla se vea el error.
 while True:
     try:
-        response = requests.get("https://es.wikipedia.org/wiki/Anexo:Sencillos_n%C3%BAmero_uno_en_Espa%C3%B1a#Canciones_con_m%C3%A1s_semanas_en_el_n%C3%BAmero_uno")
+        response = requests.get(
+            "https://es.wikipedia.org/wiki/Anexo:Sencillos_n%C3%BAmero_uno_en_Espa%C3%B1a#Canciones_con_m%C3%A1s_semanas_en_el_n%C3%BAmero_uno"
+        )
         time.sleep(5)
         break
     except Exception as e:
@@ -15,13 +18,12 @@ while True:
 soup = BeautifulSoup(response.text, "html.parser")
 
 
-#Busca la tabla y acceder al contenido.
-tabla = soup.find('table', {'class': 'wikitable'})
-cuerpo = tabla.find('tbody')
+# Busca la tabla y acceder al contenido.
+tabla = soup.find("table", {"class": "wikitable"})
+cuerpo = tabla.find("tbody")
 
 # Extraer todas las filas, ignorando la primera linea, que simplemente son los encabezados.
-filas = cuerpo.find_all('tr')[1:]
-
+filas = cuerpo.find_all("tr")[1:]
 
 
 datos = []
@@ -29,16 +31,16 @@ ultimo_puesto = None
 ultimo_semanas = None
 
 for fila in filas:
-    celdas = fila.find_all('td')
+    celdas = fila.find_all("td")
     texto_celdas = [celda.get_text(strip=True) for celda in celdas]
-    
+
     # Rellenar con valores anteriores si faltan datos
     if len(texto_celdas) == 6:
         # Fila completa: tomar todos los datos
         ultimo_puesto = texto_celdas[0]
         titulo, interprete, año, semanas, pais = texto_celdas[1:]
         ultimo_semanas = semanas
-    elif len(texto_celdas) == 5: 
+    elif len(texto_celdas) == 5:
         titulo, interprete, año, semanas, pais = texto_celdas
         ultimo_puesto = ultimo_puesto  # Se conserva
         ultimo_semanas = semanas
@@ -51,12 +53,12 @@ for fila in filas:
     mitad = len(pais) // 2
     if len(pais) % 2 == 0 and pais[:mitad] == pais[mitad:]:
         pais[:mitad]
-    
+
     # Caso 2: duplicación de palabras o frases (ColombiaColombiaTrinidad y TobagoTrinidad y Tobago)
-    
+
     # Intenta dividir el texto por mayúsculas (cuando no hay espacios)
-    partes = re.findall(r'[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(?:\s[A-ZÁÉÍÓÚÑ]?[a-záéíóúñ]+)*', pais)
-    
+    partes = re.findall(r"[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(?:\s[A-ZÁÉÍÓÚÑ]?[a-záéíóúñ]+)*", pais)
+
     resultado = []
     i = 0
     while i < len(partes):
@@ -67,16 +69,47 @@ for fila in filas:
             resultado.append(partes[i])
             i += 1
     pais = " ".join(resultado)
-    
-
+    interprete = interprete.split("con")[0]
     datos.append((titulo, interprete, año[:4], semanas, pais))
 
-#crear conexión y base de datos
+# crear conexión y base de datos
 conn = sqlite3.connect("Top_Canciones_Numero_uno_en_españa.db")
 cursor = conn.cursor()
 cursor.execute("DROP TABLE IF EXISTS canciones_con_mas_semanas_en_el_numero_uno")
-cursor.execute("CREATE TABLE IF NOT EXISTS canciones_con_mas_semanas_en_el_numero_uno(titulo text,interprete text, año int,semanas int,pais text)")
-cursor.executemany("INSERT INTO canciones_con_mas_semanas_en_el_numero_uno values (?,?,?,?,?)", datos)
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS canciones_con_mas_semanas_en_el_numero_uno(titulo text,interprete text, año int,semanas int,pais text)"
+)
+cursor.executemany(
+    "INSERT INTO canciones_con_mas_semanas_en_el_numero_uno values (?,?,?,?,?)", datos
+)
+conn.execute(
+    "ALTER TABLE canciones_con_mas_semanas_en_el_numero_uno ADD COLUMN idioma TEXT"
+)
+conn.execute(
+    "ALTER TABLE canciones_con_mas_semanas_en_el_numero_uno ADD COLUMN continente TEXT"
+)
+conn.execute(
+    """
+    UPDATE canciones_con_mas_semanas_en_el_numero_uno
+    SET continente = CASE
+        WHEN pais LIKE 'España%' THEN 'Europa'
+        WHEN pais LIKE 'Suecia%' THEN 'Europa'
+        WHEN pais LIKE 'Estados Unidos%' THEN 'América del Norte'
+        WHEN pais LIKE 'Francia%' THEN 'Europa'
+        WHEN pais LIKE 'Reino Unido%' THEN 'Europa'
+        WHEN pais LIKE 'Argentina%' THEN 'América del Sur'
+        WHEN pais LIKE 'Colombia%' THEN 'América del Sur'
+        WHEN pais LIKE 'Cuba%' THEN 'América del Norte'
+        WHEN pais LIKE 'Alemania%' THEN 'Europa'
+        WHEN pais LIKE 'Brasil%' THEN 'América del Sur'
+        WHEN pais LIKE 'Venezuela%' THEN 'América del Sur'
+        WHEN pais LIKE 'Guyana%' THEN 'América del Sur'
+        WHEN pais LIKE 'Puerto Rico%' THEN 'América del Norte'
+        WHEN pais LIKE 'Canadá%' THEN 'América del Norte'
+        ELSE 'Desconocido'
+    END;
+    """
+)
 conn.commit()
 conn.close()
 
